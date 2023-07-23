b68ac527191efb3d65f0834fcdb7fb28b302ed90
cvusmo <blacksheepcosmo@gmail.com>
2023-07-23 13:18:22 -0500

    Adding GameState checks and various other checks for the vessel to improve performance.

--- FFTProject/FFTPlugin.cs
+++ FFTProject/FFTPlugin.cs
@@ -1,13 +1,14 @@
 ﻿using BepInEx;
-using UnityEngine;
-using UnityEngine.Sprites;
-using HarmonyLib;
 using SpaceWarp;
+using KSP.Game;
+using KSP.Sim.impl;
+using BepInEx.Logging;
 using SpaceWarp.API.Mods;
+using UnityEngine;
+using KSP.Messages.PropertyWatchers;
 
 namespace FFT
 {
-
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
     public class FFTPlugin : BaseSpaceWarpPlugin
@@ -15,19 +16,49 @@ namespace FFT
         public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
         public const string ModName = MyPluginInfo.PLUGIN_NAME;
         public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
+
+        internal GameInstance _gameInstance;
+        internal VesselComponent _vesselComponent;
+        private IsActiveVessel _isActiveVessel;
+        internal TriggerController TriggerController { get; private set; } 
         public static FFTPlugin Instance { get; set; }
+
+        internal new static ManualLogSource Logger { get; set; }
         public static string Path { get; private set; }
 
         public override void OnPreInitialized()
         {
             FFTPlugin.Path = this.PluginFolderPath;
         }
+
         public override void OnInitialized()
         {
             base.OnInitialized();
 
+            Instance = this;
+            Logger = base.Logger;
+            Logger.LogInfo("Loaded");
+
+            _gameInstance = GameManager.Instance.Game;
+            GameObject controllableObject = GameObject.Find("CV401");
+            TriggerController = controllableObject.AddComponent<TriggerController>();
+            _isActiveVessel = new IsActiveVessel();
+        }
+        public void FixedUpdate()
+        {
+            GameState? state = BaseSpaceWarpPlugin.Game?.GlobalGameState?.GetState();
+
+            if (state == GameState.Launchpad || state == GameState.FlightView || state == GameState.Runway)
+            {
+                TriggerController.IsActive = true;
+                Logger.LogInfo("TriggerController IsActive = True");
+            }
+            else
+            {
+                TriggerController.IsActive = false;
+                Logger.LogInfo("TriggerController IsActive = False");
+            }
         }
         public override void OnPostInitialized() => base.OnPostInitialized();
-
     }
 }
--- FFTProject/Modules/TriggerController.cs
+++ FFTProject/Modules/TriggerController.cs
@@ -1,15 +1,17 @@
 using KSP.Animation;
 using KSP.Game;
+using KSP.Messages.PropertyWatchers;
 using KSP.Modules;
 using KSP.Sim.ResourceSystem;
 using UnityEngine;
-
 public class TriggerController : MonoBehaviour
 {
     private Module_ResourceCapacities moduleResourceCapacities;
     private TriggerVFXFromAnimation triggerVFX;
     private ResourceDefinitionID fuelResourceId;
     private Animator animator;
+    private IsActiveVessel _isActiveVessel;
+    public bool IsActive { get; set; }
 
     private void Start()
     {
@@ -18,12 +20,18 @@ public class TriggerController : MonoBehaviour
         ResourceDefinitionDatabase definitionDatabase = GameManager.Instance.Game.ResourceDefinitionDatabase;
         fuelResourceId = definitionDatabase.GetResourceIDFromName("Methalox"); // Replace "Fuel" with the actual name of your fuel resource.
         animator = GetComponent<Animator>();
+        _isActiveVessel = new IsActiveVessel();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         IResourceContainer fuelContainer = moduleResourceCapacities.OABPart.Containers[0];
         float fuelLevel = (float)fuelContainer.GetResourceStoredUnits(fuelResourceId);
         animator.SetFloat("FuelLevel", fuelLevel);
+
+        IsActive = _isActiveVessel.GetValueBool();
+
+        if (!IsActive)
+            return;
     }
 }
