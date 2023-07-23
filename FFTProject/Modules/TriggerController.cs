db3bb260247e2368793d9f6847c765f357ee7f85
cvusmo <blacksheepcosmo@gmail.com>
2023-07-23 16:02:12 -0500

    Added Emission

--- FFTProject/Modules/TriggerController.cs
+++ FFTProject/Modules/TriggerController.cs
@@ -4,16 +4,27 @@ using KSP.Messages.PropertyWatchers;
 using KSP.Modules;
 using KSP.Sim.ResourceSystem;
 using UnityEngine;
+using BepInEx.Logging;
+using FFT;
+using KSP.Sim.impl;
+using static KSP.Api.UIDataPropertyStrings.View;
+
 public class TriggerController : MonoBehaviour
 {
     private Module_ResourceCapacities _moduleResourceCapacities;
     private TriggerVFXFromAnimation _triggerVFX;
     private ResourceDefinitionID _fuelResourceId;
     private Animator _animator;
-    private IsActiveVessel _isActiveVessel;
-    private bool _wasActive;
-    private bool _coolingVFXOFF = false;
     public ParticleSystem _particleSystem;
+    private IsActiveVessel _isActiveVessel;
+    public ViewController _viewController;
+    internal VesselComponent _vesselComponent;
+    private bool _wasActive;
+    private bool _coolingVFXON;
+    private bool _coolingVFXLOOP;
+    private bool _coolingVFXOFF = false;
+    private string _activeVesselGuid = "";
+    internal new static ManualLogSource Logger { get; set; }
     public bool IsActive { get; set; }
 
     private void Start()
@@ -21,11 +32,13 @@ public class TriggerController : MonoBehaviour
         _moduleResourceCapacities = GetComponent<Module_ResourceCapacities>();
         _triggerVFX = GetComponentInChildren<TriggerVFXFromAnimation>();
         ResourceDefinitionDatabase definitionDatabase = GameManager.Instance.Game.ResourceDefinitionDatabase;
-        _fuelResourceId = definitionDatabase.GetResourceIDFromName("Methalox"); // Replace "Fuel" with the actual name of your fuel resource.
+        _fuelResourceId = definitionDatabase.GetResourceIDFromName("Methalox"); // Replace "Fuel" with the actual fuel resource.
         _animator = GetComponent<Animator>();
         _isActiveVessel = new IsActiveVessel();
 
-        Debug.Log("TriggerController has started.");
+        Logger = FFTPlugin.Logger;
+
+        Logger.LogInfo("TriggerController has started.");
     }
     public void EnableEmission()
     {
@@ -41,21 +54,35 @@ public class TriggerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+
         IResourceContainer fuelContainer = _moduleResourceCapacities.OABPart.Containers[0];
+
+        Logger.LogInfo("fuelContainer: " + fuelContainer);
+
+        _vesselComponent = GameManager.Instance?.Game?.ViewController?.GetActiveVehicle(true)?.GetSimVessel(true);
+
+
         float fuelLevel = (float)fuelContainer.GetResourceStoredUnits(_fuelResourceId);
         _animator.SetFloat("FuelLevel", fuelLevel);
 
+        Logger.LogInfo("fuelLevel: " + fuelLevel);
+
         bool _isActive = _isActiveVessel.GetValueBool();
+
+        Logger.LogInfo("_isActive: " + _isActive);
+        Logger.LogInfo("_wasActive: " + _wasActive);
 
         if (_isActive && !_wasActive)
         {
             if (fuelLevel > 0.8f)
             {
                 _animator.Play("CoolingVFX_ON");
+                Logger.LogInfo("CoolingVFX_ON: " + _coolingVFXON);
             }
             else
             {
                 _animator.Play("CoolingVFX_LOOP");
+                Logger.LogInfo("CoolingVFX_LOOP: " + _coolingVFXLOOP);
             }
 
             _coolingVFXOFF = false;
@@ -66,6 +93,7 @@ public class TriggerController : MonoBehaviour
             {
                 _animator.Play("CoolingVFX_OFF");
                 _coolingVFXOFF = true;
+                Logger.LogInfo("CoolingVFX_OFF: " + _coolingVFXOFF);
             }
         }
 
