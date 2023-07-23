5d62bffc65f683cd17d9834857a6d60f34c08ad8
cvusmo <blacksheepcosmo@gmail.com>
2023-07-23 10:45:17 -0500

    -Removed Particle System -Simplified script
    MUST setup unity animations and particlesystems correctly for this script to work.

--- FFTProject/Modules/TriggerController.cs
+++ FFTProject/Modules/TriggerController.cs
@@ -10,16 +10,14 @@ public class TriggerController : MonoBehaviour
     private TriggerVFXFromAnimation triggerVFX;
     private ResourceDefinitionID fuelResourceId;
     private Animator animator;
-    private ParticleSystem particleSystem;
 
     private void Start()
     {
         moduleResourceCapacities = GetComponent<Module_ResourceCapacities>();
-        triggerVFX = GetComponent<TriggerVFXFromAnimation>();
+        triggerVFX = GetComponentInChildren<TriggerVFXFromAnimation>();
         ResourceDefinitionDatabase definitionDatabase = GameManager.Instance.Game.ResourceDefinitionDatabase;
         fuelResourceId = definitionDatabase.GetResourceIDFromName("Methalox"); // Replace "Fuel" with the actual name of your fuel resource.
         animator = GetComponent<Animator>();
-        particleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Update()
@@ -27,28 +25,5 @@ public class TriggerController : MonoBehaviour
         IResourceContainer fuelContainer = moduleResourceCapacities.OABPart.Containers[0];
         float fuelLevel = (float)fuelContainer.GetResourceStoredUnits(fuelResourceId);
         animator.SetFloat("FuelLevel", fuelLevel);
-
-        if (fuelLevel > 0.8)
-        {
-            triggerVFX.VFX01_ON();
-
-            if (particleSystem != null)
-            {
-                particleSystem.Play();
-            }
-            Debug.Log("VFX01_ON called");
-            animator.SetBool("IsFuelAbove80", true);
-        }
-        else
-        {
-            triggerVFX.VFX01_OFF();
-
-            if (particleSystem != null)
-            {
-                particleSystem.Stop();
-            }
-            Debug.Log("VFX01_OFF called");
-            animator.SetBool("IsFuelAbove80", false);
-        }
     }
 }
