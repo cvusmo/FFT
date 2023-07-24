bb12636834fcbca276efe61061d7198ceb57935e
cvusmo <blacksheepcosmo@gmail.com>
2023-07-24 17:42:21 -0500

    Moved the IsActive property definition from within the FixedUpdate method to the class level
    Adjusted the setter in the IsActive property to handle cases when the value changes
    
    Simplified the FixedUpdate method

--- FFTProject/Modules/TriggerController.cs
+++ FFTProject/Modules/TriggerController.cs
@@ -1,13 +1,13 @@
 ﻿using BepInEx.Logging;
 using FFT;
-using KSP.Messages.PropertyWatchers;
 using KSP.Animation;
 using KSP.Game;
+using KSP.Messages.PropertyWatchers;
 using KSP.Modules;
 using KSP.Sim.impl;
 using KSP.Sim.ResourceSystem;
+using SpaceWarp.API.Game;
 using UnityEngine;
-using System.Linq;
 
 public class TriggerController : MonoBehaviour
 {
@@ -15,23 +15,22 @@ public class TriggerController : MonoBehaviour
     private Animator _animator;
     private ParticleSystem _particleSystem;
     private IsActiveVessel _isActiveVessel;
+    private VesselComponent _vesselComponent;
     private GameObject _coolingVFX;
     private VesselFuelLevelPropertyWatcher _fuelLevelWatcher;
     private Module_ResourceCapacities _moduleResourceCapacities;
     private ResourceDefinitionID _fuelResourceId;
     private ResourceDataProvider _resourceDataProvider;
-
     private bool _wasActive;
     private bool _coolingVFXOFF = false;
+    public bool _isActive;
 
     internal new static ManualLogSource Logger { get; set; }
-    public bool IsActive { get; set; }
+    //public bool IsActive { get; set; }
 
     private void Start()
     {
         Logger = FFTPlugin.Logger;
-
-        _resourceDataProvider = new ResourceDataProvider();
 
         _triggerVFX = GetComponent<TriggerVFXFromAnimation>();
         if (_triggerVFX == null)
@@ -63,16 +62,19 @@ public class TriggerController : MonoBehaviour
 
         _isActiveVessel = new IsActiveVessel();
         _fuelLevelWatcher = new VesselFuelLevelPropertyWatcher();
+        _resourceDataProvider = new ResourceDataProvider();
+        _vesselComponent = new VesselComponent();
+        _moduleResourceCapacities = new Module_ResourceCapacities();
+        _fuelResourceId = new ResourceDefinitionID();
+
 
         Logger.LogInfo("TriggerController has started.");
     }
-
     public void EnableEmission()
     {
         var emission = _particleSystem.emission;
         emission.enabled = true;
     }
-
     public void DisableEmission()
     {
         var emission = _particleSystem.emission;
@@ -82,76 +84,94 @@ public class TriggerController : MonoBehaviour
     {
         _particleSystem.Play();
     }
-
     public void StopParticleSystem()
     {
         _particleSystem.Stop();
     }
-
-    private void FixedUpdate()
+    public bool IsActive
     {
-        GameState? state = FFTPlugin.Instance.GetGameState();
-
-        IResourceContainer fuelContainer = _moduleResourceCapacities.OABPart.Containers[0];
-        Logger.LogInfo("fuelContainer: " + fuelContainer);
-
-        float fuelLevel = (float)fuelContainer.GetResourceStoredUnits(_fuelResourceId);
-        _animator.SetFloat("FuelLevel", fuelLevel);
-
-        if (_animator == null)
+        get { return _isActive; }
+        set
         {
-            _animator = GetComponentInParent<Animator>();
-            if (_animator == null)
+            _isActive = value;
+            if (_isActive)
             {
-                Logger.LogError("Animator not found in parent GameObject");
-                return;
-            }
-        }
-
-        if (_fuelLevelWatcher != null)
-        {
-            fuelLevel = (float)(_fuelLevelWatcher.GetValueDouble() / 100.0);
-            _animator.SetFloat("FuelLevel", fuelLevel);
-        }
-
-        bool _isActive = _isActiveVessel.GetValueBool();
-
-        if (_isActive)
-        {
-            if (!_wasActive)
-            {
-                _coolingVFXOFF = false;
-                EnableEmission();
-                if (fuelLevel > 0.8f)
+                if (!_wasActive && FuelLevelExceedsThreshold())
                 {
+                    EnableEmission();
+                    _triggerVFX.enabled = true;
+                    StartParticleSystem();
                     _animator.Play("CoolingVFX_ON");
                     Logger.LogInfo("CoolingVFX_ON: ");
                 }
             }
-            else if (fuelLevel > 0.8f && !_animator.GetCurrentAnimatorStateInfo(0).IsName("CoolingVFX_LOOP"))
+            else
             {
-                _animator.Play("CoolingVFX_LOOP");
-                Logger.LogInfo("CoolingVFX_LOOP: ");
-                StartParticleSystem();
-            }
-            else if (fuelLevel <= 0.8f && !_coolingVFXOFF && _animator.GetCurrentAnimatorStateInfo(0).IsName("CoolingVFX_LOOP"))
-            {
-                _animator.Play("CoolingVFX_OFF");
-                _coolingVFXOFF = true;
-                Logger.LogInfo("CoolingVFX_OFF: " + _coolingVFXOFF);
-                StopParticleSystem();
                 DisableEmission();
+                _triggerVFX.enabled = false;
             }
+
+            _wasActive = _isActive;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        VesselComponent _vesselComponent = Vehicle.ActiveSimVessel;
+        _vesselComponent.RefreshFuelPercentages();
+
+        IResourceContainer fuelContainer = _moduleResourceCapacities.OABPart.Containers[0];
+        Logger.LogInfo("fuelContainer: " + fuelContainer);
+
+        UpdateFuelLevel(fuelContainer);
+
+        IsActive = _isActiveVessel.GetValueBool();
+
+        if (IsActive && _wasActive)
+        {
+            HandleActiveVessel(fuelContainer);
         }
         else if (_wasActive && !_coolingVFXOFF)
         {
-            _animator.Play("CoolingVFX_OFF");
-            _coolingVFXOFF = true;
-            Logger.LogInfo("CoolingVFX_OFF: " + _coolingVFXOFF);
-            StopParticleSystem();
-            DisableEmission();
+            DisableCoolingEffects();
         }
-
-        _wasActive = _isActive;
     }
+
+    private void UpdateFuelLevel(IResourceContainer _fuelContainer)
+    {
+        float fuelLevel = _fuelLevelWatcher != null ? (float)(_fuelLevelWatcher.GetValueDouble() / 100.0) : (float)_fuelContainer.GetResourceStoredUnits(_fuelResourceId);
+        _animator.SetFloat("FuelLevel", fuelLevel);
+    }
+
+    private bool FuelLevelExceedsThreshold()
+    {
+        return _animator.GetFloat("FuelLevel") > 0.8f;
+    }
+
+    private void HandleActiveVessel(IResourceContainer _fuelContainer)
+    {
+        if (FuelLevelExceedsThreshold() && !_animator.GetCurrentAnimatorStateInfo(0).IsName("CoolingVFX_LOOP"))
+        {
+            _triggerVFX.enabled = true;
+            StartParticleSystem();
+            _animator.Play("CoolingVFX_LOOP");
+            Logger.LogInfo("CoolingVFX_LOOP: ");
+        }
+        else if (!FuelLevelExceedsThreshold() && !_coolingVFXOFF && _animator.GetCurrentAnimatorStateInfo(0).IsName("CoolingVFX_LOOP"))
+        {
+            DisableCoolingEffects();
+        }
+    }
+
+    private void DisableCoolingEffects()
+    {
+        _animator.Play("CoolingVFX_OFF");
+        _coolingVFXOFF = true;
+        _triggerVFX.enabled = true;
+        StopParticleSystem();
+        DisableEmission();
+        Logger.LogInfo("CoolingVFX_OFF: " + _coolingVFXOFF);
+    }
+
+
 }
