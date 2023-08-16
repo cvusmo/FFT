b5d7e80354b08868a6678e5f94231a0fa81dd1de
cvusmo <blacksheepcosmo@gmail.com>
2023-08-16 17:09:48 -0500

    Added config file

--- FFTProject/FFTPlugin.cs
+++ FFTProject/FFTPlugin.cs
@@ -1,7 +1,13 @@
-﻿using BepInEx;
+﻿//|=====================Summary========================|0|
+//|                Main plugin for FFT                 |1|
+//|by cvusmo===========================================|4|
+//|====================================================|1|
+
+using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
+using FFT.Controllers;
 using FFT.Managers;
-using FFT.Modules;
 using SpaceWarp;
 using SpaceWarp.API.Mods;
 
@@ -14,19 +20,27 @@ namespace FFT
         public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
         public const string ModName = MyPluginInfo.PLUGIN_NAME;
         public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
-        public FFT.Managers.Manager manager { get; private set; }
+
+        public ConfigEntry<bool> FFTConfig;
+        internal Manager manager { get; private set; }
+        internal ConditionsManager conditionsmanager { get; private set; }
+        internal MessageManager messagemanager { get; private set; }
+        internal LoadModule LoadModule { get; private set; }
         public new static ManualLogSource Logger { get; set; }
         public static string Path { get; private set; }
         public override void OnInitialized()
         {
             FFTPlugin.Path = this.PluginFolderPath;
             base.OnInitialized();
-            MessageManager.Instance.SubscribeToMessages();
+            messagemanager.SubscribeToMessages();
             Logger = base.Logger;
             Logger.LogInfo("Loaded");
 
-            manager = new FFT.Managers.Manager();
-
+            // configuration
+            FFTConfig = Config.Bind
+                ("Fancy Fuel Tanks Settings",
+                "Fancy Fuel Tanks v0.1.4.1",
+                LoadModule.EnableVFX, "Fancy Fuel Tanks adds Dynamic Environmental Effects to fuel tanks");
             UpdateConditions();
         }
         public void UpdateConditions()
